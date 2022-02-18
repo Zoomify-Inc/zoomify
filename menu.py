15cb60624af966048abed7ed20d19a3f64c2bf2e
Ediberto Ponce <ediberto01@gmail.com>
2022-02-17 21:21:12 -0800

    Refactored code, testing simple_term_menu library

--- menu.py
+++ menu.py
@@ -3,25 +3,57 @@ import click
 from colorama import init, Fore, Back, Style
 import sys
 from getpass import getpass
+# from simple_term_menu import TerminalMenu
 
 # @click.command()
 # def home_menu():
 #   click.secho(pyfiglet.figlet_format("Welcome to Zoomify's"), fg="green", bold=False)
 #   click.secho(pyfiglet.figlet_format("Attendance Checker"), fg="red", bold=False)
 
-def message(str):
+def message(str, color=Fore.RED):
   message = pyfiglet.figlet_format(str)
   init(autoreset=True)
-  print(Style.BRIGHT + Fore.RED + message)
+  print(Style.BRIGHT + color + message)
+
+# def test_menu():
+#   command_options_test = ["[r] apple", "[v] banana", "[e] orange"]
+#   terminal_menu = TerminalMenu(command_options_test, title="command_options_test")
+#   menu_entry_index = terminal_menu.show()
+
+  # if terminal_menu == 'r':
+  #   attendance_report()
+  # elif terminal_menu == 'v':
+  #   data_visualization()
+  # elif terminal_menu == 'e':
+  #   exit()
+  # else: 
+  #   print('you have entered a invalid option please try again.')
+
+def command_options():
+  command_options = '''
+  Command Options:
+
+  - Check Attendance Report: "R"
+  - Data Visualization Tool: "V"
+  - Exit Program: "E"
+  '''
+  print(command_options)
+  option = input("Please Enter a Command > ")
+
+  if option.lower() == 'r':
+    attendance_report()
+  elif option.lower() == 'v':
+    data_visualization()
+  elif option.lower() == 'e':
+    exit()
+  else: 
+    print('you have entered a invalid option please try again.')
+
   
 def welcome():
   message("Welcome to Zoomify's Attendance Checker")
 
-# option = input("Enter L to login or E to exit: ")
-
-
 def welcome_menu():
-  # print('PLEASE ENTER "L" TO LOGIN')
   option = input("Enter L to login or E to exit: ")
   while option.lower() != 'e':
     if option.lower() == 'l':
@@ -48,68 +80,19 @@ def main_menu():
   ******************
   Instructions: After the end of your meeting, you have access to some useful commands shown below!
   '''
-  # instructions = 'Instructions: After the end of your meeting, you have access to some useful commands shown below!'
   print(title)
-
-  command_options = '''
-  Command Options:
-
-  - Check Attendance Report: "R"
-  - Data Visualization Tool: "V"
-  - Exit Program: "E"
-  '''
-  print(command_options)
-  option = input("Please Enter a Command > ")
-
-  if option.lower() == 'r':
-    attendance_report()
-  elif option.lower() == 'v':
-    data_visualization()
-  elif option.lower() == 'e':
-    exit()
-  else: 
-    print('you have entered a invalid option please try again.')
-
-
+  command_options()
 
 
 def attendance_report():
-  print("this is your attendance report!")
-  command_options = '''
-  Command Options:
-  - Go Back: "B"
-  - Exit Program: "E"
-  '''
-
-  print(command_options)
-  option = input("Please Enter a Command > ")
-
-  if option.lower() == 'b':
-    main_menu()
-  if option.lower() == 'e':
-    exit()
-  else: 
-    print('you have entered a invalid option please try again.')
-
+  message("Attendance Report", Fore.BLUE)
+  command_options()
 
 
 def data_visualization():
-  print("DATA VISUALIZATION!")
-  command_options = '''
-  Command Options:
-  - Go Back: "B"
-  - Exit Program: "E"
-  '''
+  message("Data Visualization", Fore.BLUE)
+  command_options()
 
-  print(command_options)
-  option = input("Please Enter a Command > ")
-
-  if option.lower() == 'b':
-    main_menu()
-  elif option.lower() == 'e':
-    exit()
-  else: 
-    print('you have entered a invalid option please try again.')
 
 def exit():
   message('Thank you for using Zoomify')
@@ -134,3 +117,4 @@ if __name__ == '__main__':
   welcome()
   welcome_menu()
   main_menu()
+  # test_menu()

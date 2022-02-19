dd9be7e0e37c6e3481e1552c26b72c1ad216d06a
DrPayne25 <alex.payne1125@gmail.com>
2022-02-19 10:01:48 -0800

    alex's notes for each function we worked on

--- menu.py
+++ menu.py
@@ -3,6 +3,7 @@ import click
 from colorama import init, Fore, Back, Style
 import sys
 from getpass import getpass
+from simple_term_menu import TerminalMenu
 
 # @click.command()
 # def home_menu():

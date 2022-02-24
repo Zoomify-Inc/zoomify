6667f8bed4212fcbc6fcb1a06bc169d5ec371728
Alexander Payne <81712870+DrPayne25@users.noreply.github.com>
2022-02-23 21:45:37 -0800

    tests for menu passing now with 95% woot woot

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -1,3 +1,4 @@
+from cmath import exp
 import imp
 import pytest 
 from menu import Menu
@@ -27,10 +28,104 @@ def test_welcome():
     menu = Menu()
 
     try:
+      menu.get_env_vars()
       menu.welcome()
       menu.welcome_menu()
     except SystemExit:
         real_print("Exiting is allowed")
+    expected_lines = parse_expected_lines(path='tests/exit_menu_sim.txt', sample='')
+    diff = find_differences(text, expected_lines)
+    assert diff != None
+
+def test_email_and_exit():
+    inputs = ['l', 'idkburkes@gmail.com', '0', 'e']
+    text = ''
+
+    def mock_input(*args):
+        return inputs.pop(0)
+    # store the "real" print & input so we can restore them later
+    real_input = builtins.input
+    # mock the built in print & input
+    builtins.input = mock_input
+
+    def mock_print(*args):
+        nonlocal text
+        text += "".join(args) + "\n"
+    real_print = builtins.print
+    # mock the built in print & input
+    builtins.print = mock_print
+    menu = Menu()
+
+    try:
+      menu.get_env_vars()
+      menu.welcome()
+      menu.welcome_menu()
+    except SystemExit:
+        real_print("Exiting is allowed")
+    expected_lines = parse_expected_lines(path='tests/login_and_exit.txt', sample='')
+    diff = find_differences(text, expected_lines)
+    assert diff != None
+
+def test_sheet_export():
+    inputs = ['l', 'idkburkes@gmail.com', '0', 'r', '19', 'g','e']
+    text = ''
+
+    def mock_input(*args):
+        return inputs.pop(0)
+    # store the "real" print & input so we can restore them later
+    real_input = builtins.input
+    # mock the built in print & input
+    builtins.input = mock_input
+
+    def mock_print(*args):
+        nonlocal text
+        text += "".join(args) + "\n"
+    real_print = builtins.print
+    # mock the built in print & input
+    builtins.print = mock_print
+    menu = Menu()
+
+    try:
+      menu.get_env_vars()
+      menu.welcome()
+      menu.welcome_menu()
+    except SystemExit:
+        real_print("Exiting is allowed")
+    expected_lines = parse_expected_lines(path='tests/login_report_sheets_sim.txt', sample='')
+    diff = find_differences(text, expected_lines)
+    assert diff != None
+
+def test_attendance_report():
+    inputs = ['l', 'idkburkes@gmail.com', '0', 'r', '19','e']
+    text = ''
+
+    def mock_input(*args):
+        return inputs.pop(0)
+    # store the "real" print & input so we can restore them later
+    real_input = builtins.input
+    # mock the built in print & input
+    builtins.input = mock_input
+
+    def mock_print(*args):
+        nonlocal text
+        text += "".join(args) + "\n"
+    real_print = builtins.print
+    # mock the built in print & input
+    builtins.print = mock_print
+    menu = Menu()
+
+    try:
+      menu.get_env_vars()
+      menu.welcome()
+      menu.welcome_menu()
+    except SystemExit:
+        real_print("Exiting is allowed")
+    expected_lines = parse_expected_lines(path='tests/login_report_sheets_sim.txt', sample='')
+    diff = find_differences(text, expected_lines)
+    assert diff != None
+
+
+
 
 def find_differences(text, expected_lines):
     actual_lines = text.splitlines()
